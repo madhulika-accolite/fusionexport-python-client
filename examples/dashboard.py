e9a3ab58278c42ae65547615ec42b40d6ae00a50
Madhulika Mukherjee <madhulika.m@accolite.com>
2022-01-16 18:57:39 +0530

    fusiontime export example

--- examples/dashboard.py
+++ examples/dashboard.py
@@ -6,16 +6,21 @@ from fusionexport import ExportManager, ExportConfig  # Import sdk
 # Instantiate the ExportConfig class and add the required configurations
 export_config = ExportConfig()
 
-export_config["chartConfig"] = "resources/multiple.json"
+#export_config["chartConfig"] = "resources/multiple.json"
 
-export_config["templateFilePath"] = "resources/template.html"
-export_config["type"] = "png"
-export_config["templateFormat"] = "A1"
-export_config["templateHeight"] = 600
-
+#export_config["templateFilePath"] = "resources/template.html"
+export_config["templateFilePath"] = "resources/fusiontime.html"
+#export_config["templateFilePath"] = "resources/template_timeseries.html"
+#export_config["templateFilePath"] = "resources/all_charts.html"
+export_config["type"] = "pdf"
+#export_config["templateFormat"] = "A4"
+#export_config["templateWidth"]= "1500"
+#export_config["templateHeight"] = "1600"
+export_config["asyncCapture"] = True
+export_config["maxWaitForCaptureExit"]=8000
 
 # Provide port and host of FusionExport Service
-export_server_host = "127.0.0.1"
+export_server_host ="127.0.0.1"
 export_server_port = 1337
 
 # Instantiate the ExportManager class
